ef79c53254ed1c41f58c57934588927174cf6738
Ashmitha C <124855048+Ashmitha14@users.noreply.github.com>
2024-03-01 16:23:29 +0530

    Add files via upload

---
+++ 727722euit021/Day 8/demo8/src/main/java/com/example/demo8/repository/PersonRepo.java
@@ -0,0 +1,17 @@
+package com.example.demo8.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo8.model.Person;
+
+import java.util.List;
+
+
+@Repository
+public interface PersonRepo extends JpaRepository<Person,Integer>{
+
+    List<Person> findByNameStartingWith(String name);
+    List<Person> findByNameEndingWith(String name);
+    
+}
---
+++ 727722euit021/Day 8/demo8/src/main/java/com/example/demo8/repository/ProductRepo.java
@@ -0,0 +1,17 @@
+package com.example.demo8.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo8.model.Product;
+
+import java.util.List;
+
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer>{
+
+    List<Product> findByProductNameStartingWith(String name);
+    List<Product> findByProductNameEndingWith(String name);
+    
+}
